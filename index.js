ed949f4662f87e79cd163691f4445314eac201d9
github username <github email address>
2019-01-26 16:34:48 +0000

    learn submit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+    }
+    return arr 
+}
+
+function coin() {
+    return Math.random() >= 0.5;
+}
+
+const tailsNeverFails = () => {
+    let count = 0; 
+    while(coin()) {
+        count++
+    }
+    return `You got ${count} tails in a row!`
+}
